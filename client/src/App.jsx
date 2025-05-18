95d3572c2ea3c44506cdc06f4ceed4f0331316a0
Jason Smith <jasondsmith2501@gmail.com>
2025-05-18 16:15:13 -0700

    Added baseline integration for Auth0 as a PoC

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    isLoading,
+    error,
+    isAuthenticated,
+    loginWithRedirect,
+    logout
+  } = useAuth0();
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isAuthenticated) {
+    return (
+      <button onClick={() => {
+        logout({
+          logoutParams: {
+            returnTo: window.location.origin
+          }
+        });
+      }}>Log out</button>
+    );
+  } else {
+    return (
+      <button onClick={loginWithRedirect}>Log in</button>
+    );
+  }
 }
 
 export default App
