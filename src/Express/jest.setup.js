aae7f188c6bb941c804e44d4ef753619fccbf455
Jason Smith <jasondsmith2501@gmail.com>
2025-05-17 17:02:05 -0700

    Centralized setup in one file

--- src/Express/jest.setup.js
+++ src/Express/jest.setup.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const app = require('./app');
-const getTestToken = require("./tests/getTestToken");
+const axios = require("axios");
+
+async function getTestToken() {
+    const res = await axios.post(`${process.env.ISSUER_BASE_URL}/oauth/token`, {
+        client_id: process.env.AUTH0_CLIENT_ID,
+        client_secret: process.env.AUTH0_CLIENT_SECRET,
+        audience: process.env.AUDIENCE,
+        grant_type: "client_credentials"
+    });
+
+    return res.data.access_token;
+}
 
 /**
  * Create an in memory Mongo DB database and launch
