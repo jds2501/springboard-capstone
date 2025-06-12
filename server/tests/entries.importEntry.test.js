58f4cb628b4bd272879f3b48ffe20c179fd94437
Jason Smith <jasondsmith2501@gmail.com>
2025-06-11 22:47:29 -0700

    Added a basic sanity import test

--- server/tests/entries.importEntry.test.js
+++ server/tests/entries.importEntry.test.js
@@ -20,7 +20,6 @@ describe("Entries Import Entry API Tests", () => {
         title: Test Entry
         date: 2023-06-01
         ---
-        
         This is the body of the test entry.
         `);
 
@@ -32,7 +31,14 @@ describe("Entries Import Entry API Tests", () => {
         Buffer.from(markdownContent, "utf-8"),
         "imported-entry.md"
       );
-    console.log(res.body);
+
     expect(res.statusCode).toBe(201);
+    expect(res.body).toHaveProperty("id");
+    expect(res.body).toHaveProperty("title", "Test Entry");
+    expect(res.body).toHaveProperty("date", "2023-06-01T00:00:00.000Z");
+    expect(res.body).toHaveProperty(
+      "description",
+      "This is the body of the test entry."
+    );
   });
 });
